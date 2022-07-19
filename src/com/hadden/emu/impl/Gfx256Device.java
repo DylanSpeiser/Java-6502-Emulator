0d219426d8d1f0657d8e280ea8ced0ad4008969a
miketbush <miketbush@gmail.com>
2022-07-18 21:37:45 -0500

    Add more ports for special gfx function

--- src/com/hadden/emu/impl/Gfx256Device.java
+++ src/com/hadden/emu/impl/Gfx256Device.java
@@ -21,6 +21,14 @@ public class Gfx256Device extends JFrame implements BusDevice, HasPorts, ActionL
 	private static final int CONST_HEIGHT_SIZE 	= 480;
 	private static final int CONST_PAGE_SIZE 	= 0x2800;
 
+	
+	private static final int CONST_RENDER_REG	= 0x00;
+	private static final int CONST_X_HIGH		= 0x01;
+	private static final int CONST_X_LOW			= 0x02;
+	private static final int CONST_Y_HIGH		= 0x03;
+	private static final int CONST_Y_LOW			= 0x04;
+	private static final int CONST_BANK_REG   	= 0x05;
+	
 	private Stack repaints = new Stack();
 	
 	GfxPanel gfxp = new GfxPanel();
@@ -47,18 +55,77 @@ public class Gfx256Device extends JFrame implements BusDevice, HasPorts, ActionL
 
 	private int basePort = 0;
 
-	private BusAddressRange barPort = null;
+	private BusAddressRange[] barPort = new BusAddressRange[0x06];
 
 	private int portValue = 0;
+	private int portValueLast = 0;
 
+	final class GfxPort implements BusDevice
+	{
+		private String name;
+		private int port;
+		private BusAddressRange bar;
+		private int portValue = 0;
+		
+		public GfxPort(String name, int port, BusAddressRange bar)
+		{
+			this.name = name;
+			this.port = port;
+			this.bar  = bar;
+		}
+		
+		@Override
+		public String getName()
+		{
+			return name;
+		}
+
+		@Override
+		public BusAddressRange getBusAddressRange()
+		{
+			return bar;
+		}
+
+		@Override
+		public void writeAddress(int address, int value, IOSize size)
+		{
+			if(address == port)
+				portValue = value;				
+		}
+
+		@Override
+		public int readAddressSigned(int address, IOSize size)
+		{
+			if(address == port)
+				return portValue;
+			
+			return 0;			
+		}
+
+		@Override
+		public int readAddressUnsigned(int address, IOSize size)
+		{
+			if(address == port)
+				return portValue;
+			
+			return 0;		
+		}
+	}
+	
 	public Gfx256Device(int baseAddress)
 	{
 		this.setSize(CONST_WIDTH_SIZE, CONST_HEIGHT_SIZE);
 
 		this.baseAddress = baseAddress;
-		this.basePort  = 0x0000B00F;
+		this.basePort  = 0x0000B00A;
 		this.bar = new BusAddressRange(baseAddress,CONST_PAGE_SIZE,1);
-		this.barPort  = new BusAddressRange(basePort,1,1);
+		this.barPort[CONST_RENDER_REG]  = new BusAddressRange(basePort + CONST_RENDER_REG,1,1);
+		this.barPort[CONST_X_HIGH]      = new BusAddressRange(basePort + CONST_X_HIGH,1,1);
+		this.barPort[CONST_X_LOW]       = new BusAddressRange(basePort + CONST_X_LOW,1,1);
+		this.barPort[CONST_Y_HIGH]      = new BusAddressRange(basePort + CONST_Y_HIGH,1,1);
+		this.barPort[CONST_Y_LOW]       = new BusAddressRange(basePort + CONST_Y_LOW,1,1);
+		this.barPort[CONST_BANK_REG]    = new BusAddressRange(basePort + CONST_BANK_REG,1,1);
+		
 		this.portValue  = 0;
 
 		
@@ -76,21 +143,21 @@ public class Gfx256Device extends JFrame implements BusDevice, HasPorts, ActionL
 		
 		Color palette[] = {
 							Color.BLACK,
-				           Color.BLACK,
-				           Color.BLACK,
-						   Color.DARK_GRAY,
-						   Color.GRAY,
-						   Color.LIGHT_GRAY,
-						   Color.RED,
-						   Color.GREEN,
-						   Color.BLUE,
-						   Color.CYAN,
-						   Color.ORANGE,
-						   Color.YELLOW,
-						   Color.MAGENTA,
-						   Color.PINK,
-						   Color.WHITE,
-						   Color.WHITE
+							Color.DARK_GRAY,
+				            	Color.BLACK,
+						    Color.DARK_GRAY,
+						    Color.GRAY,
+						    Color.LIGHT_GRAY,
+						    Color.RED,
+						    Color.GREEN,
+						    Color.BLUE,
+						    Color.CYAN,
+						    Color.ORANGE,
+						    Color.YELLOW,
+						    Color.MAGENTA,
+						    Color.PINK,
+						    Color.DARK_GRAY,
+						    Color.WHITE
 						  };
 		
 		public GfxPanel()
@@ -103,16 +170,16 @@ public class Gfx256Device extends JFrame implements BusDevice, HasPorts, ActionL
 		//public void paintComponent(Graphics g)
 		public void paint(Graphics g)
 		{
-			super.paint(g);
+			//super.paint(g);
 			System.out.println("paintComponent");
-			//g.setColor(Color.BLACK);
-			//g.fillRect(0, 0, gfxp.getWidth(), gfxp.getHeight());
-			//for(int p=0;p<30;p++)
-			
-			if(!repaints.empty())
+			g.setColor(Color.BLACK);
+			g.fillRect(0, 0, gfxp.getWidth(), gfxp.getHeight());
+			for(int p=0;p<30;p++)
+			//
+			//if(!repaints.empty())
 			{
-				int p = ((Integer)repaints.pop()).intValue();
-				System.out.println("Page:" + p);
+				//int p = portValueLast; //((Integer)repaints.pop()).intValue();
+				//System.out.println("Page:" + p);
 				for(int i=0;i<banks[p].length;i++)
 				{			
 					if(banks[p][i] != 0)
@@ -145,47 +212,14 @@ public class Gfx256Device extends JFrame implements BusDevice, HasPorts, ActionL
 	@Override
 	public BusDevice[] ports(int baseAddress)
 	{
-		BusDevice[] ports = {new BusDevice() 
-		{
-
-			@Override
-			public String getName()
-			{
-				return "Gfx256Port";
-			}
-
-			@Override
-			public BusAddressRange getBusAddressRange()
-			{
-				return barPort;
-			}
-
-			@Override
-			public void writeAddress(int address, int value, IOSize size)
-			{
-				if(address == basePort)
-					portValue = value;				
-			}
-
-			@Override
-			public int readAddressSigned(int address, IOSize size)
-			{
-				if(address == basePort)
-					return portValue;
-				
-				return 0;			
-			}
-
-			@Override
-			public int readAddressUnsigned(int address, IOSize size)
-			{
-				if(address == basePort)
-					return portValue;
-				
-				return 0;		
-			}
-			
-		}};
+		BusDevice[] ports = {
+		new GfxPort("GFX-256-RENDERPORT",basePort + CONST_RENDER_REG,barPort[CONST_RENDER_REG]), //this.barPort[CONST_RENDER_REG]  = new BusAddressRange(basePort + CONST_RENDER_REG,1,1);
+		new GfxPort("GFX-256-XHIPORT",basePort    + CONST_X_HIGH,barPort[CONST_X_HIGH]), //this.barPort[CONST_X_HIGH]      = new BusAddressRange(basePort + CONST_X_HIGH,1,1);
+		new GfxPort("GFX-256-XLOPORT",basePort    + CONST_X_LOW,barPort[CONST_X_LOW]), //this.barPort[CONST_X_LOW]       = new BusAddressRange(basePort + CONST_X_LOW,1,1);
+		new GfxPort("GFX-256-YHIPORT",basePort    + CONST_Y_HIGH,barPort[CONST_Y_HIGH]), //this.barPort[CONST_Y_HIGH]      = new BusAddressRange(basePort + CONST_Y_HIGH,1,1);
+		new GfxPort("GFX-256-YLOPORT",basePort    + CONST_Y_LOW,barPort[CONST_Y_LOW]), //this.barPort[CONST_Y_LOW]       = new BusAddressRange(basePort + CONST_Y_LOW,1,1);
+		new GfxPort("GFX-256-BANKPORT",basePort   + CONST_BANK_REG,barPort[CONST_BANK_REG]), //this.barPort[CONST_BANK_REG]    = new BusAddressRange(basePort + CONST_BANK_REG,1,1);		
+		};
 		return ports;
 	}	
 	
@@ -198,15 +232,31 @@ public class Gfx256Device extends JFrame implements BusDevice, HasPorts, ActionL
 	@Override
 	public void writeAddress(int address, int value, IOSize size)
 	{
-		if(address == basePort)
+		try
 		{
-			portValue = value;
-			return;
+			if(address == (basePort + CONST_BANK_REG))
+			{
+				if(portValue!=value)
+				{
+					portValueLast = portValue;
+					//gfxp.repaint();
+				}
+				portValue = value;
+				//repaints.push(portValue);
+				
+				return;
+			}
+			banks[portValue][this.bar.getRelativeAddress(address)] = (byte)value;
+			//repaints.push(portValue);
+			//System.out.println("Push Page:" + portValue);
+		} 
+		catch (Exception e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		banks[portValue][this.bar.getRelativeAddress(address)] = (byte)value;
-		repaints.push(portValue);
-		System.out.println("Push Page:" + portValue);
-		gfxp.repaint();
+		
+		//gfxp.repaint();
 	}
 
 	@Override
